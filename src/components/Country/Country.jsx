4f8da629b1eb5ec86da754eb463c38eb21642ebb
mamun-prodhan <mamunpr9@gmail.com>
2023-09-14 09:50:49 +0600

    conditional rendering added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Country.css";
 
 const Country = ({ country }) => {
   const { name, flags, population, area } = country;
+  const [isVisited, setIsVisited] = useState(false);
+
+  const handleClick = () => {
+    setIsVisited(!isVisited);
+  };
   return (
     <div className="country">
       <h3>Name: {name.common}</h3>
       <img src={flags.png} alt="flags" />
       <p>Population: {population}</p>
       <p>Area: {area}</p>
+      <button onClick={handleClick} className="button">
+        {isVisited ? "Visited" : "Will Visit"}
+      </button>
+      {isVisited ? "I have Visited" : "I will Visit"}
     </div>
   );
 };
