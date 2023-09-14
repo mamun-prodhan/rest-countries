b34aeb89fdf308684b9078ee01c141ff8af16de7
mamun-prodhan <mamunpr9@gmail.com>
2023-09-14 08:17:23 +0600

    country data loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Countries from "./components/Countries/Countries";
+
 function App() {
   return (
     <>
       <h1 className="text-center">React World Tour</h1>
+      <Countries></Countries>
     </>
   );
 }
